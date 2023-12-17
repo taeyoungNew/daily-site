1a995764e4a0721306d08b1942177882ab1315b9
taeyoungNew <dndbxhd10@naver.com>
2023-12-18 02:08:12 +0900

    [feat] 메인페이지 게시물 무한스크롤기능 추가

--- front/src/api/post.js
+++ front/src/api/post.js
@@ -1,7 +1,20 @@
 import axios from "axios";
 export default class PostApi {
-  loadPosts = async () => {
-    return await axios.get(`http://localhost:3000/api/post/`);
+  loadPosts = async (lastId) => {
+    return await axios.get(
+      `http://localhost:3000/api/post?lastId=${lastId}&limit=5`
+      // { withCredentials: true }
+    );
+    // return await axios.get(
+    //   {
+    //     methods: "get",
+    //     url: `http://localhost:3000/api/post/`,
+    //     params: {
+    //       lastId: lastId,
+    //     },
+    //   },
+    //   { withCredentials: true }
+    // );
   };
 
   getMyPosts = async () => {
@@ -11,6 +24,7 @@ export default class PostApi {
   };
 
   createPost = async (payload) => {
+    console.log(payload);
     return await axios.post(`http://localhost:3000/api/post/`, payload, {
       withCredentials: true, // axios는 withCredentials를 true로 하지 않으면 토큰을 저장안함
     });
